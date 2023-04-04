1ba03649c43fd900905f46ef834f4abc59aacd03
GoItTutor <114779287+GoItTutor@users.noreply.github.com>
2023-04-04 13:53:17 +0200

    Initialize module-09-lesson-01

---
+++ src/js/async.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+ * Синхронний vs Асинхронний JS
+ */
+
+// Стек виклику функцій
+// http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
+
+// Функції відкладеного виклику window.setTimeout(callback, delay, arg1, arg2, ...);
+
+// Очищення інтервалів і таймаутів clearInterval(intervalID), clearTimeout(timeoutID)
