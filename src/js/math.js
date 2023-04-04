eb534735e567c0e16c37b18be2a020ab6b656ff7
GoItTutor <114779287+GoItTutor@users.noreply.github.com>
2023-04-04 13:53:16 +0200

    Initialize module-08-lesson-02

---
+++ src/js/math.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const division = (a, b) => {
+  return a / b;
+};
+
+const multi = (a, b) => {
+  return a * b;
+};
