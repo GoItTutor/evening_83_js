dbba3bcaf35fa57c890611fed13180bb996805c3
GoItTutor <sheva09a@gmail.com>
2023-04-18 20:32:45 +0200

    module-03-lesson-01

--- index.js
+++ index.js
@@ -11,6 +11,60 @@
   - Перебір for...in
 */
 
+// const isMale = true;
+
+// const user1 = {
+//   eyeColor: "green",
+//   hairColor: "brown",
+//   isMale,
+//   // height: 181,
+//   // adress: {
+//   //   country: "Ukraine",
+//   // },
+//   // friends: ["Anya", "Sofiya", "Vasyl"],
+//   // isMale: true,
+// };
+
+// const user2 = user1;
+
+// console.log(user1);
+// console.log(user2);
+
+// console.log(user1 === user2);
+
+// user2.country = "Ukraine";
+
+// console.log(user1);
+// console.log(user2);
+
+// let a = 1;
+// let b = a;
+
+// console.log(a);
+// console.log(b);
+
+// a = 10;
+
+// console.log(a);
+// console.log(b);
+
+// user.weight = 75;
+
+// user.height = 200;
+
+// const key = "heig";
+
+// user[key + "ht"] = 180;
+
+// console.log(user);
+// console.log(user["isMale"]);
+
+// for (let key of user) {
+//   console.log(key);
+
+//   console.log(user[key]);
+// }
+
 /*
   2. Напиши скрипт, який для об'єкта user послідовно:
   
@@ -22,10 +76,30 @@
 */
 
 // const user = {
-//   hobby: 'guitar',
+//   hobby: "guitar",
 //   premium: true,
-//   occupation: 'software engineer',
+//   occupation: "software engineer",
 // };
+
+// user.mood = "happy";
+
+// user.hobby = "skydiving";
+
+// user.premium = false;
+
+// console.log(user);
+
+// for (let key in user) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+
+// const keysArray = Object.keys(user);
+
+// for (let key of keysArray) {
+//   console.log(`${key}: ${user[key]}`);
+// }
+
+// console.log(keysArray);
 
 /*
   3. Метод Object.values()
@@ -40,10 +114,24 @@
 */
 
 // const salaries = {
-//   Andrii: 3000,
-//   Anna: 1200,
-//   Sofiya: 2000,
+//   // Andrii: 3000,
+//   // Anna: 1200,
+//   // Sofiya: 2000,
 // };
+
+// function getResult(salaries) {
+//   const values = Object.values(salaries);
+
+//   let salariesSum = 0;
+
+//   for (let salary of values) {
+//     salariesSum += salary;
+//   }
+
+//   return salariesSum;
+// }
+
+// console.log(getResult(salaries));
 
 /*
   4. Масив об'єктів
@@ -55,12 +143,24 @@
 */
 
 // const stones = [
-//   { name: 'Смарагд', price: 1300, quantity: 4 },
-//   { name: 'Діамант', price: 2700, quantity: 3 },
-//   { name: 'Сапфір', price: 400, quantity: 7 },
-//   { name: 'Топаз', price: 500, quantity: 10 },
-//   { name: 'Аквамарин', price: 200, quantity: 8 },
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 3 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Топаз", price: 500, quantity: 10 },
+//   { name: "Аквамарин", price: 200, quantity: 8 },
 // ];
+
+// function calcTotalPrice(stonesArr, stoneName) {
+//   for (let stoneObj of stonesArr) {
+//     if (stoneObj.name.toLowerCase() === stoneName.toLowerCase()) {
+//       return stoneObj.price * stoneObj.quantity;
+//     }
+//   }
+
+//   return `Каміння з ім'ям ${stoneName} не має в наявності`;
+// }
+
+// console.log(calcTotalPrice(stones, "ТоПаз"));
 
 /*
   5. Методи об'єкта та робота з this
@@ -74,17 +174,69 @@
 // Типів транзакцій лише два:
 // Можна покласти чи зняти гроші з балансу
 const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
 };
 
-const account = {
+const account1 = {
   // Поточний баланс
   balance: 0,
 
   // Исторія транзакцій
   transactions: [],
+
+  createTransaction(amount, type) {
+    return { amount, type, id: Math.random() };
+  },
+
+  deposit(amount) {
+    this.balance += amount;
+
+    const newTransaction = this.createTransaction(amount, Transaction.DEPOSIT);
+
+    this.transactions.push(newTransaction);
+  },
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      console.log(`Зняття суми ${amount} не можливе - недостатньо коштів`);
+
+      return;
+    }
+
+    this.balance -= amount;
+
+    const newTransaction = this.createTransaction(amount, Transaction.WITHDRAW);
+
+    this.transactions.push(newTransaction);
+  },
 };
+
+// console.log(account);
+
+// account.deposit(5000);
+
+// account.withdraw(3000);
+
+// console.log(account);
+
+// const account2 = {
+//   balance: 0,
+//   transactions: [],
+//   createTransaction: account1.createTransaction,
+//   deposit: account1.deposit,
+//   withdraw: account1.withdraw,
+
+//   showBalance() {
+//     console.log(this.balance);
+//   },
+// };
+
+// console.log(account2.balance);
+
+// account2.deposit(13000);
+
+// console.log(account2.balance);
 
 // Методи які необхнідно реалізувати:
 
@@ -116,3 +268,27 @@ const account = {
   Викликає createTransaction для створення об'єкта транзакції
   та занесення його в історію транзакцій.
 */
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+
+//   addPotion(newPotion) {
+//     if (this.potions.includes(newPotion)) {
+//       return `Error! Potion ${newPotion} is already in your inventory!`;
+//     }
+//   },
+// };
+
+// const key = prompt(`key`);
+
+// const value = prompt(`value`);
+
+// const obj = {};
+
+// obj[key] = value;
+
+// console.log(obj);
