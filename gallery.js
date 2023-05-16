56d91f46e860b07b2e2394404d406fd570db5170
GoItTutor <sheva09a@gmail.com>
2023-05-16 20:41:11 +0200

    module-06-lesson-01

--- gallery.js
+++ gallery.js
@@ -10,6 +10,27 @@
   </li>
 */
 
+// const galleryEl = document.querySelector(".gallery");
+
+// const listItemEl = document.createElement("li");
+
+// listItemEl.classList.add("gallery-item");
+
+// const anchorLinkEl = document.createElement("a");
+
+// anchorLinkEl.href = "#";
+
+// listItemEl.append(anchorLinkEl);
+
+// const imgEl = document.createElement("img");
+
+// imgEl.src = "https://picsum.photos/id/237/200/300";
+// imgEl.alt = "Labrador";
+
+// anchorLinkEl.append(imgEl);
+
+// galleryEl.prepend(listItemEl);
+
 /* 
   6. Властивості innerHtml і textContent
 
@@ -22,6 +43,15 @@
     </a>
   </li>
 */
+
+// const galleryEl = document.querySelector(".gallery");
+
+// galleryEl.innerHTML += `<li class="gallery-item">
+//     <h1>Some text</h1>
+//     <a href="#">
+//       <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+//     </a>
+//   </li>`;
 
 /*
   7. Створити галерею і вставити у елемент з класом gallery через
@@ -36,40 +66,75 @@
   </li>
 */
 
+// const galleryEl = document.querySelector(".gallery");
+
+// galleryEl.children[1].insertAdjacentHTML(
+//   `afterbegin`,
+//   `<li class="gallery-item">
+//     <a href="#">
+//       <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+//     </a>
+//   </li>`
+// );
+
 const pictures = [
   {
     width: 700,
     height: 460,
-    url: 'https://picsum.photos/id/0/700/460',
-    alt: 'laptop',
+    url: "https://picsum.photos/id/0/700/460",
+    alt: "laptop",
   },
   {
     width: 700,
     height: 460,
-    url: 'https://picsum.photos/id/1/700/460',
-    alt: 'developer',
+    url: "https://picsum.photos/id/1/700/460",
+    alt: "developer",
   },
   {
     width: 700,
     height: 460,
-    url: 'https://picsum.photos/id/10/700/460',
-    alt: 'forest',
+    url: "https://picsum.photos/id/10/700/460",
+    alt: "forest",
   },
   {
     width: 700,
     height: 460,
-    url: 'https://picsum.photos/id/100/700/460',
-    alt: 'beach',
+    url: "https://picsum.photos/id/100/700/460",
+    alt: "beach",
   },
   {
     width: 700,
     height: 460,
-    url: 'https://picsum.photos/id/1000/700/460',
-    alt: 'mountain',
+    url: "https://picsum.photos/id/1000/700/460",
+    alt: "mountain",
   },
 ];
 
 // Написати функцію яка створить розмітку картки
-const makeGalleryCard = ({ width, height, url, alt }) => {};
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeGalleryCard = ({
+  width,
+  height,
+  url,
+  alt,
+}) => `<li class="gallery-item">
+    <a href="#">
+      <img src="${url}" alt="${alt}" width="${width}" height="${height}"> 
+    </a>
+  </li>`;
 
 // Cтворити масив із розмітки і вставити у список .gallery
+
+// const galleryCardsList = pictures.map((el) => makeGalleryCard(el));
+
+// galleryEl.insertAdjacentHTML("afterbegin", galleryCardsList.join(""));
+
+const listItemsEls = document.querySelectorAll(".item");
+
+console.log(listItemsEls);
+
+listItemsEls.forEach((el) => {
+  console.log(el);
+});
--- index.js
+++ index.js
@@ -10,6 +10,20 @@
   Навігація по DOM дереву (children, etc.)
 */
 
+// const paragraphEl = document.querySelector(".description");
+
+// console.log((paragraphEl.style.color = "red"));
+
+// const listItemsEls = document.querySelectorAll(".fail");
+
+// console.log(listItemsEls);
+
+// const containerEl = document.querySelector(".container");
+
+// console.dir(containerEl);
+
+// console.dir(containerEl.firstElementChild.parentElement);
+
 /* 
   2. Доступ до атрибутів у елемента
 
@@ -24,7 +38,17 @@
   show on img with class some-img
 */
 
-// const imgEl = document.querySelector('.some-img');
+// const imgEl = document.querySelector(".some-img");
+
+// console.log(imgEl.getAttribute("checked"));
+
+// imgEl.setAttribute("src", "https://picsum.photos/id/155/200/300");
+
+// imgEl.removeAttribute("src");
+
+// console.log(imgEl.title);
+
+// imgEl.src = "https://picsum.photos/id/156/200/300";
 
 /*
   3. Об'єкт style, властивість cssText
@@ -32,8 +56,28 @@
   show on heading with class main-title
 */
 
+// const headingEl = document.querySelector(".main-title");
+
+// headingEl.style.color = "green";
+// headingEl.style.backgroundColor = "red";
+
+// headingEl.style.cssText = "color: green; background-color: red";
+
 /*
   4. Об'єкт classList та його методи (add, remove, toggle, contains)
 
   take dissapear and red classes from main.css
 */
+
+const imgEl = document.querySelector(".some-img");
+
+console.log(imgEl);
+
+// imgEl.classList.add("dissapear");
+
+// imgEl.classList.remove("dissapear");
+
+// imgEl.classList.toggle("dissapear");
+// imgEl.classList.toggle("dissapear");
+
+console.log(imgEl.classList.contains("somedasdas-img"));
